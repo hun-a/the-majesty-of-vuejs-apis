a0bd77223fe436347583c724e76598ac8d1c359e
seunghun <seunghun.chan@gmail.com>
2019-06-23 23:11:49 +0900

    Implement the API - GET /api/stories/:id

--- api/index.js
+++ api/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const stories = require("./stories");
 
 router.get("/stories", stories.getAll);
+router.get("/stories/:id", stories.getById);
 
 module.exports = router;
--- api/stories.js
+++ api/stories.js
@@ -1,13 +1,27 @@
 const R = require("ramda");
-const models = require("../data/models");
+const { Stories } = require("../data/models");
 
 const getAll = (req, res, next) =>
-  models.Stories.findAll({
+  Stories.findAll({
     attributes: ["id", "plot", "upvotes", "writer"]
   })
     .then(result => res.json(result))
     .catch(next);
 
+const getById = (req, res, next) => {
+  const {id} = req.params;
+  if (!id) {
+    return next("Id is required.");
+  }
+
+  Stories.findOne({
+    where: { id },
+    attributes: ["id", "plot", "upvotes", "writer"]
+  })
+    .then(result => res.json(result))
+    .catch(next);
+};
+
 module.exports = {
-  getAll
+  getAll, getById
 };
