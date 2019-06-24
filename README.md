18bbb02beb31aedcc4909c091ecac4a9d6dacd8e
huna <huna@wemakeprice.com>
2019-06-24 14:24:05 +0900

    Implement the API for pagination ex

--- README.md
+++ README.md
@@ -16,3 +16,61 @@ $ cd the-majesty-of-vuejs-apis
 $ npm install
 $ npm run start
 ```
+
+# Note
+
+If you pass no parameters when call the `GET /api/stories`, you can get datas like as below format.
+
+```
+[
+  {
+    id: 1,
+    plot: "Ahh, hard to see, the Dark Side is.",
+    upvotes: 421,
+    writer: "Yoda"
+  },
+  {
+    id: 2,
+    plot: "The Force is strong with you. A powerful Sith you will become. Henceforth, you shall be known as Darth… Vader.",
+    upvotes: 729,
+    writer: " Darth Sidious"
+  },
+  {
+    id: 3,
+    plot: "Mmm. Lost a planet, Master Obi-Wan has. How embarrassing.",
+    upvotes: 800,
+    writer: "Yoda"
+  },
+  ...
+]
+```
+
+But if you pass the `page` parameter into the  `GET /api/stories` then  you can get datas for pagination like as below format.
+
+```
+{
+  total: 170,
+  per_page: 15,
+  current_page: 1,
+  last_page: 12,
+  next_page_url: 2,
+  prev_page_url: null,
+  from: 1,
+  to: 15,
+  data: [
+    {
+      id: 2,
+      plot: "The Force is strong with you. A powerful Sith you will become. Henceforth, you shall be known as Darth… Vader.",
+      upvotes: 729,
+      writer: " Darth Sidious"
+    },
+    {
+      id: 3,
+      plot: "Mmm. Lost a planet, Master Obi-Wan has. How embarrassing.",
+      upvotes: 800,
+      writer: "Yoda"
+    },
+    ...
+  ]
+}
+```
--- api/stories.js
+++ api/stories.js
@@ -1,12 +1,46 @@
 const R = require("ramda");
 const { Stories } = require("../data/models");
 
-const getAll = (req, res, next) =>
-  Stories.findAll({
-    attributes: ["id", "plot", "upvotes", "writer"]
-  })
-    .then(result => res.json(result))
-    .catch(next);
+const getAll = (req, res, next) => {
+  if (req.query.page) {
+    const current_page = Number(req.query.page);
+    const per_page = Number(req.query.per_page) || 15;
+    const from = (current_page - 1) * per_page + 1;
+    const to = from + per_page - 1;
+
+    Stories.count()
+      .then(total => {
+        const last_page = Math.ceil(total / per_page);
+        const next_page_url = current_page < last_page ? current_page + 1 : null;
+        const prev_page_url = current_page == 1 ? null : current_page - 1
+
+        return Stories.findAll({
+            attributes: ["id", "plot", "upvotes", "writer"],
+            offset: from,
+            limit: per_page
+          })
+            .then(data => res.json({
+              total,
+              per_page,
+              current_page,
+              last_page,
+              next_page_url,
+              prev_page_url,
+              from,
+              to,
+              data
+            }))
+            .catch(next);
+      })
+      .catch(next);
+  } else {
+    Stories.findAll({
+      attributes: ["id", "plot", "upvotes", "writer"]
+    })
+      .then(data => res.json(data))
+      .catch(next);
+  }
+};
 
 const getById = (req, res, next) => {
   const { params: { id } } = req;
